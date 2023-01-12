349ddd9c060afbd925a9947db78885c9cd36cda1
Biswajeet <122534291+biswajeetgithub2@users.noreply.github.com>
2023-01-12 18:13:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('My Parents New Healthy diner') 
 streamlit.Header('This is very interesting')
-streamlit.text('Lets Start the New Things !')
+streamlit.text('Lets Start the New Things')
