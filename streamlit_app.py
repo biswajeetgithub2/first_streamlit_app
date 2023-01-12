2b145b5ddb47b9ddcc603dd1ffda4c2f9c0792ae
Biswajeet <122534291+biswajeetgithub2@users.noreply.github.com>
2023-01-12 19:31:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
-import pandas
+import streamlit
+streamlit.title('My Parents New Healthy diner') 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
-fruit_listed = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-streamlit.dataframe(fruit_listed)
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
